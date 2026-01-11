4a583ce3cc694e27484b563b7bc91d30482cbf2a
isaacdizolele-open <isaac.dizolele@open-co.com>
2026-01-10 21:23:05 -0300

    Adicionar AuthProvider para disponibilizar contexto de autenticação em toda a aplicação

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+// NOVO: Import do Provider de autenticação
+import { AuthProvider } from './contexts/AuthContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* NOVO: AuthProvider envolve toda aplicação para disponibilizar contexto de autenticação */}
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </StrictMode>,
 )
